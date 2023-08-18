4311c5c172859c7c8a64d52fc4fdd4fcf625dce2
Paulo Victor Lorençato <pvlorencato@gmail.com>
2023-08-18 08:31:25 -0300

    Dois primeiros exercícios

---
+++ fundamentos/secao-3-introducao-ao-javascript/dia-02-array-e-estrutura-de-repeticao/ex2.js
@@ -0,0 +1,14 @@
+// O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo:
+
+// O fatorial é representado pelo sinal !
+// Exemplo de 4 fatorial:
+// 4! = 4 x 3 x 2 x 1 = 24
+
+// Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
+let fatorial = 1;
+let operador = 10;
+for(let i=operador;i>0;i--){
+    fatorial = fatorial * i;
+}
+
+console.log(fatorial);
